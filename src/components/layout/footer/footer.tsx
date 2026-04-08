e20dedb8d973de13fba9725500b893133ae94e74
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-04-08 16:05:44 -0600

    add: header basic structure

---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <section>
+        <div></div>
+        <div></div>
+      </section>
+      <p>&copy; 2026 Polidev</p>
+    </footer>
+  );
+}
--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router";
+import "./header.css";
+
 export default function Header() {
   return (
     <header className="header">
-      <h1>Portfolio header</h1>
+      <nav className="header-nav">
+        <Link to="/">Polidev</Link>
+
+        <ul className="header-nav-list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Projects</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+          <button className="header-download-button">Download CV</button>
+        </ul>
+      </nav>
     </header>
   );
 }
