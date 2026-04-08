46c88e5e171f0c64816ad9c641af731e386ae5b0
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-04-08 15:20:06 -0600

    add: initial config

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to my Portfolio</h1>
+      <p>This is the home page of my portfolio website.</p>
+    </div>
+  );
+}
