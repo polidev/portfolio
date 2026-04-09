c92ef61857f0fcdfe03ef2554edd78c90104c43a
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-04-08 21:01:13 -0600

    add: footer basic structure

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -3,11 +3,25 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <section>
-        <div></div>
-        <div></div>
+      <section className="contact-section">
+        <div className="contact-section_left">
+          <h2>GET IN TOUCH</h2>
+          <p>
+            Available for freelance work, frontend engineering, high quality
+            UI/UX design and performance.
+          </p>
+        </div>
+
+        <div className="contact-section_right">
+          <form action="">
+            <input type="text" placeholder="Your Name" required />
+            <input type="email" placeholder="Your Email" required />
+            <textarea placeholder="Message" required></textarea>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
       </section>
-      <p>&copy; 2026 Polidev</p>
+      <p className="copyright">&copy; 2026 Polidev</p>
     </footer>
   );
 }
