6b88e2c7b1dd709ff5f6dbd00a7437a17ba85889
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-04-15 11:38:21 -0600

    add: hero animations and rework

--- src/components/ui/techStack/techStack.tsx
+++ src/components/ui/techStack/techStack.tsx
@@ -1,5 +1,6 @@
 import htmlIcon from "../../../assets/icons8-html-5.svg";
 import cssIcon from "../../../assets/icons8-css-3.svg";
+import tailwindIcon from "../../../assets/icons8-tailwind-css.svg";
 import jsIcon from "../../../assets/icons8-javascript.svg";
 import tsIcon from "../../../assets/icons8-typescript.svg";
 import reactIcon from "../../../assets/icons8-react.svg";
@@ -12,82 +13,91 @@ export default function TechStack() {
   return (
     <>
       <ul className="tech-stack">
-        <li>
+        <li className="animate-delay-100">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={htmlIcon}
             alt="HTML Icon"
             title="HTML"
           />
         </li>
-        <li>
+        <li className="animate-delay-150">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={cssIcon}
             alt="CSS Icon"
             title="CSS"
           />
         </li>
-        <li>
+        <li className="animate-delay-200">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
+            src={tailwindIcon}
+            alt="Tailwind Icon"
+            title="Tailwind"
+          />
+        </li>
+        <li className="animate-delay-250">
+          <img
+            max-width="48"
+            max-height="48"
             src={jsIcon}
             alt="JavaScript Icon"
             title="JavaScript"
           />
         </li>
-        <li>
+        <li className="animate-delay-300">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={tsIcon}
             alt="TypeScript Icon"
             title="TypeScript"
           />
         </li>
-        <li>
+        <li className="animate-delay-350">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={reactIcon}
             alt="React Icon"
             title="React"
           />
         </li>
-        <li>
+        <li className="animate-delay-400">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={nodeIcon}
             alt="Node.js Icon"
             title="Node.js"
           />
         </li>
-        <li>
+        <li className="animate-delay-450">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={expressIcon}
             alt="Express.js Icon"
             title="Express.js"
           />
         </li>
-        <li>
+        <li className="animate-delay-500">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src="https://img.icons8.com/color/48/sql.png"
-            alt="sql"
+            alt="SQL Icon"
             title="SQL"
           />
         </li>
-        <li>
+        <li className="animate-delay-550">
           <img
-            width="48"
-            height="48"
+            max-width="48"
+            max-height="48"
             src={postgreIcon}
             alt="PostgreSQL Icon"
             title="PostgreSQL"
