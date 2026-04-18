49bf764a0235e6be15540ad68340220d4a3d2f69
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-04-18 13:12:30 -0600

    add: top scroll button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/ui/hero/hero.tsx";
 import Projects from "../components/ui/projects/projects.tsx";
 import Contact from "../components/ui/contact/contact.tsx";
+import ToTop from "../components/layout/toTop/toTop.tsx";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Contact />
+      <ToTop />
     </>
   );
 }
